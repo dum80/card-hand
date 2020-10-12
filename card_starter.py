71336b589c762853a805a0d06585a3b84f718a3e
dum80 <dumbo.swe@gmail.com>
2020-10-12 18:24:12 -0400

    add get_card()

--- card_starter.py
+++
@@ -1,2 +0,0 @@
-# make_hand()
-# hand_value()
--- cards.py
+++ cards.py
@@ -1,1 +1,17 @@
 # get_card()
+import random
+
+deck = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+def get_card():
+    """
+    Parameter:
+        none
+    Return:
+        <String> a random value from a list of a deck of cards' ranks
+    """
+    # print(random.choice(deck))
+    return random.choice(deck)
+
+# help(get_card)
+# get_card()
