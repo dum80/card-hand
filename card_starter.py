d324a21131f44960a9a8384841edf7e1c802a5c1
dum80 <dumbo.swe@gmail.com>
2020-10-12 18:25:11 -0400

    import get_card(), add skeleton code

---
+++ card_starter.py
@@ -0,0 +1,19 @@
+from cards import get_card
+
+def make_hand(num):
+    """
+    Parameter:
+        num: number of cards to generate
+    Return:
+        <Sequence> of randomly generated cards using get_card() 
+    """
+    pass
+
+
+# define function hand_value
+
+
+
+
+# print(f"The Hand: {make_hand()}")     # eg. ["3", "5", "A", "5"]
+# print(f"Its Value: {hand_value()}")   $ eg. 27
